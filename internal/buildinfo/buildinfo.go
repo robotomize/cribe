ac93c583c29e1c7e8a874cd92932ec5339aa85f0
robotomize <robotomize@gmail.com>
2021-10-20 20:33:30 +0300

    fix info url

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -4,7 +4,7 @@ const (
 	Graffiti       = "             ._____.                   ___.           __   \n  ___________|__\\_ |__   ____          \\_ |__   _____/  |_ \n_/ ___\\_  __ \\  || __ \\_/ __ \\   ______ | __ \\ /  _ \\   __\\\n\\  \\___|  | \\/  || \\_\\ \\  ___/  /_____/ | \\_\\ (  <_> )  |  \n \\___  >__|  |__||___  /\\___  >         |___  /\\____/|__|  \n     \\/              \\/     \\/              \\/             "
 	GreetingCLI    = "\nversion: %s \nbuild time: %s\ntg: %s\ngithub: %s\n"
 	GithubBloopURL = "https://github.com/robotomize/cribe.git"
-	TgBloopURL     = "https://t.me/@cribe_bot"
+	TgBloopURL     = "https://t.me/cribe_bot"
 )
 
 var (
